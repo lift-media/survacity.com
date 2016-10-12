1ed89e2292828d383ffd3a2755c7e59fafec77d9
lift-media <lift-media@liftmedia.io>
2016-10-12 17:35:30 +0530

    Master Google Auth

--- public/js/app.js
+++ public/js/app.js
@@ -34041,16 +34041,13 @@ Vue.component('update-profile-details', {
     },
     ready: function ready() {
         var fromEmail = this.user.from_email;
-        if (fromEmail != "" || fromEmail != null) {
+        if (fromEmail != null) {
             $('#lblEmail').html("You are Connected as " + fromEmail + '<a href="/google" class="btn-connect btn btn-success">Connect a Different Gmail Account</a>');
         } else {
             $('#lblEmail').html('You are not connected <a href="/google" class="btn-connect btn btn-success">Connect a Gmail Account</a>');
         }
         this.form.from_email = this.user.from_email;
         this.form.signature = this.user.signature;
-        //alert(this.user.from_email+ ' : '+this.user.signature);
-        //console.log(this.user.from_email);
-        //console.log(this.user.signature);
     },
 
 
